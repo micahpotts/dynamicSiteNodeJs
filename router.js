aebfb6d9a110b016498932b83eaa1d22dc119791
Micah Potts <Micah Potts>
2017-02-07 19:06:50 -0500

    debugged code. 'footer' in header, misspelling in router

--- router.js
+++ router.js
@@ -1,29 +1,44 @@
 const Profile = require("./profile.js");
 const renderer = require("./renderer.js");
-
+var querystring = require("querystring");
+const commonHeaders = {'Content-Type': 'text/html'};
 // handle http route GET / and POST / i.e. home
 const home = (request, response) => {
   //if url == '/' && GET
   if(request.url === "/") {
-    //show url
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    renderer.view('header', {}, response);
-    renderer.view('search', {}, response);
-    renderer.view('footer', {}, response);
-    response.end();
+    if(request.method.toLowerCase() === "get") {
+      //show url
+      //response.writeHead(200, commonHeaders);
+      response.statusCode = 200;
+      response.setHeader.commonHeaders;
+      renderer.view('header', {}, response);
+      renderer.view('search', {}, response);
+      renderer.view('footer', {}, response);
+      response.end();
+    } else {
+      //if url == '/' && POST
+
+      //get the post data from body
+      request.on("data", function(postBody) {
+        //extract the username
+        var query = querystring.parse(postBody.toString());
+        response.write(query.username);
+        response.end();
+        //redirect to /:username
+      });
+    }
   }
 }
-  //if url == '/' && POST
-    //redirect to /:username
+
 
 // handle http route GET /  :usermane i.e. /micahpotts
 const user = (request, response) => {
   //if url == '/....'
   let username = request.url.replace("/", "");
   if(username.length > 0) {
+    //response.writeHead(200, commonHeaders);
     response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
+    response.setHeader.commonHeaders;
     renderer.view('header', {}, response);
 
     //get json from Treehouse
